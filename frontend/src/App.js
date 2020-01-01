7622d38311336ea354d86e219696cbf29e0da13c
HSR <hectorsilvarobles@gmail.com>
2020-01-01 10:54:46 -0700

    making some changes in package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Home} />
+        <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
       </Switch>
     </div>
