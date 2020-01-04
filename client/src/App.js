f14a5922e426eb77e838932a8bc255385e792e60
HSR <hectorsilvarobles@gmail.com>
2020-01-04 12:20:45 -0700

    Added menu and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom'
-import About from './Component/About/About'
+import Auth from './hoc/auth';
+
 import Home from './Component/Home/Home'
 import Login from './Component/LoginPage/LoginPage'
 import Register from './Component/RegisterPage/RegisterPage'
+import NavBar from './Component/NavBar/NavBar'
+
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-      </Switch>
-    </div>
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{paddingTop: '75px', minHeight: 'calc(100vh - 80px'}}>
+        <Switch>
+          <Route exact path='/' component={Auth(Home, null)} />
+          <Route path='/login' component={Auth(Login, false)} />
+          <Route path='/register' component={Auth(Register, false)} />
+        </Switch>
+      </div>
+    </Suspense>
+    
   );
 }
 
