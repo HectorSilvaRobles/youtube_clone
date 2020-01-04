b2b365d4298388e9f7fa2a9039c9f83bd4886055
HSR <hectorsilvarobles@gmail.com>
2020-01-03 21:23:45 -0700

    finishing register page

--- server/index.js
+++ server/index.js
@@ -33,7 +33,19 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', require('./routes/rUsers'));
 
+if(process.env.NODE_ENV === 'production'){
+
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+    })
+
+}
+
 const port = process.env.PORT || 6000
+
 
 
 app.listen(port, () => console.log(`server running on port ${port}`));
